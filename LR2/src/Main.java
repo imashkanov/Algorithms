cd4dc752cc92baee132b57298d638ca3408d07cd
Gleb Mashkanov <gleb.mashkanov@gmail.com>
2018-03-22 21:58:47 +0300

    added LR2 v2

--- LR2/src/Main.java
+++ LR2/src/Main.java
@@ -1,6 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 
@@ -10,16 +10,37 @@ public class Main {
     int b;
     int num;
 
+    public int cntOfFree() {
+      return n-a-b;
+    }
+
+    enum typeOfFlu { ANY, A, B }
+
+    public typeOfFlu getTypeOfFlu() {
+      if (a>0) {
+        return typeOfFlu.A;
+      }
+      if (b>0) {
+        return typeOfFlu.B;
+      }
+      return typeOfFlu.ANY;
+    }
+
     @Override
     public String toString() {
-      return String.format("[#%-5d | cap=%d | cntA=%d | cntB=%d]", num, n, a, b);
+      return String.format("[#%-5d | cap:%d  A/B: %d/%d  free:%d   %s", num, n, a, b, cntOfFree(), getTypeOfFlu());
     }
   }
+  private static final boolean DEBUG = false;
 
   static int p;
   static int needA;
   static int needB;
   static Chamber[] arr;
+  static Chamber[] arrOfNullCham;
+  static int leftA;
+  static int leftB;
+  static int allocated;
 
   public static void readDataFromFile() throws IOException {
     BufferedReader in = new BufferedReader(new FileReader("input.txt"));
@@ -32,7 +53,7 @@ public class Main {
     }
     String s;
     int idx = 0;
-    while (((s = in.readLine()) != null)) {
+    while ((idx<p) && (s = in.readLine()) != null) {
       String[] leksArr = s.split(" ");
       int cap = Integer.parseInt(leksArr[0]);
       int a = Integer.parseInt(leksArr[1]);
@@ -48,66 +69,141 @@ public class Main {
     in.close();
   }
 
-  public static void calc1() {
-    int allocatedA;
-    int allocatedB;
-    int leftA = needA;
-    int leftB = needB;
-    for (Chamber chamber: arr) {
-      if (chamber.a + chamber.b == 0) {
-        continue;
+  public  static  void  sort() {
+    Arrays.sort(arr, new Comparator<Chamber>() {
+      @Override
+      public int compare(Chamber c1, Chamber c2) {
+        int cnt1 = c1.cntOfFree();
+        int cnt2 = c2.cntOfFree();
+        return Integer.compare(cnt2, cnt1);
       }
-      if (chamber.a != 0) {
-        int aPut = chamber.n-chamber.a; //возможно разместить столько больных типа А в данной палате
-        if (aPut>0 && aPut<=leftA) {
-          chamber.a+=aPut;
-          leftA-=aPut;
-        }
-        continue;
-      }
-      if (chamber.b != 0) {
-        int bPut = chamber.n-chamber.b; //возможно разместить столько больных типа B в данной палате
-        if (bPut>0 && bPut<=leftB) {
-          chamber.b+=bPut;
-          leftB-=bPut;
-        }
-        continue;
-      }
-    }
-    System.out.printf("leftA=%d, leftB=%d\n", leftA, leftB);
-    printTable();
-    for (Chamber chamber: arr) {
-      if (chamber.a + chamber.b != 0) {
-        continue;
-      }
-      if (leftA >= chamber.n) {
-        chamber.a = chamber.n;
-        leftA -= chamber.n;
-        continue;
-      }
-      if (leftB >= chamber.n) {
-        chamber.b = chamber.n;
-        leftB -= chamber.n;
-        continue;
-      }
-    }
-    System.out.printf("leftA=%d, leftB=%d\n", leftA, leftB);
-    printTable();
-
+    });
+    printTable("After sort data:");
   }
 
-  public static void printTable() {
+
+  public static void calc() {
+    leftA = needA;
+    leftB = needB;
+
+    //заполнение непустых палат
+    for (Chamber c : arr) {
+      if (c.getTypeOfFlu() == Chamber.typeOfFlu.ANY) {
+        continue;
+      }
+      //размещаем в непустую палату тип А
+      if (c.getTypeOfFlu() == Chamber.typeOfFlu.A) {
+        if (c.cntOfFree() > 0) {
+          int aPut = Math.min(leftA, c.cntOfFree());
+          c.a += aPut;
+          leftA -= aPut;
+          if (DEBUG)
+           System.out.printf("*%d* A->%d (%d)\n", c.num, aPut, leftA);
+          continue;
+        }
+      }
+      //размещаем в непустую палату тип В
+      if (c.getTypeOfFlu() == Chamber.typeOfFlu.B) {
+        if (c.cntOfFree() > 0) {
+          int bPut = Math.min(leftB, c.cntOfFree());
+          c.b += bPut;
+          leftB -= bPut;
+          if (DEBUG)
+           System.out.printf("*%d* B->%d (%d)\n", c.num, bPut, leftB);
+          continue;
+        }
+      }
+    }
+
+    //заполнение пустых палат
+    for (Chamber c : arr) {
+      if (c.getTypeOfFlu() != Chamber.typeOfFlu.ANY) {
+        continue;
+      }
+      //размещаем в пустую палату тип А
+      if (leftA > leftB) {
+        if (c.cntOfFree() > 0) {
+          int aPut = Math.min(leftA, c.cntOfFree());
+          c.a += aPut;
+          leftA -= aPut;
+          if (DEBUG)
+            System.out.printf("*%d* A=>%d (%d)\n", c.num, aPut, leftA);
+          continue;
+        }
+      } else {
+        //иначе размещаем в пустую палату тип В
+        if (c.cntOfFree() > 0) {
+          int bPut = Math.min(leftB, c.cntOfFree());
+          c.b += bPut;
+          leftB -= bPut;
+          if (DEBUG)
+            System.out.printf("*%d* B=>%d (%d)\n", c.num, bPut, leftB);
+          continue;
+        }
+      }
+    } //for
+    allocated = needA+needB-leftB-leftA;
+
+    printTable("After allocation:");
+    if (DEBUG)
+     System.out.printf("leftA=%d, leftB=%d\n", leftA, leftB);
+  }
+
+
+  public static void printTable(String caption) {
+    if (!DEBUG)
+      return;
+    System.out.println();
+    System.out.println(caption);
+    System.out.println("----------");
     for(Chamber c: arr) {
       System.out.printf("%s\n", c);
     }
   }
 
+  public static void writeDataToFile() throws IOException {
+    Chamber[] arrOfA;
+    BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+    writer.write(Integer.toString(allocated));
+    if (allocated == needA + needB) {
+      writer.write("\n");
+      arrOfA = sortFilteredArrByCap();
+      StringBuilder sb = new StringBuilder();
+      for (Chamber c: arrOfA) {
+        sb.append(c.num);
+        sb.append(" ");
+      }
+      String s = sb.toString().trim();
+      writer.write(s);
+      if (DEBUG)
+        System.out.println(s);
+    }
+    writer.close();
+  }
+
+  //сортировка по номеру палаты
+  public static Chamber[] sortFilteredArrByCap() {
+    Chamber[] res = Arrays.stream(arr).filter(x -> (x.a !=0)).toArray(Chamber[]::new);
+    Arrays.sort(res, new Comparator<Chamber>() {
+      @Override
+      public int compare(Chamber o1, Chamber o2) {
+        return Integer.compare(o1.num, o2.num);
+      }
+    });
+    return res;
+  }
+
   public static void main(String[] args) {
     try {
+      long l = System.currentTimeMillis() ;
       readDataFromFile();
-      System.out.printf("needA = %d, needB = %d, cntCham = %d\n", needA, needB, p);
-      printTable();
-      calc1();
+      System.out.printf("need alloc: %d needA:%d, needB:%d, cntCham:%d\n", needA+needB, needA, needB, p);
+      printTable("Source data:");
+      sort();
+      calc();
+      writeDataToFile();
+      System.out.printf("allocated = %d\n", allocated);
+      System.out.println(String.format("%d ms end", System.currentTimeMillis() - l));
     } catch (Exception e) {
       e.printStackTrace();
     }
