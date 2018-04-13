4cba3a9383fb5575a70b3bb45c619188f77de5e0
Gleb Mashkanov <gleb.mashkanov@gmail.com>
2018-04-13 21:43:01 +0300

    added LR6 v1

--- LR6/src/Main.java
+++ LR6/src/Main.java
@@ -1,13 +1,17 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import com.sun.corba.se.impl.oa.poa.POAImpl;
 
-public class Main {
+import java.io.*;
+import java.util.ArrayList;
 
-  static class Point {
+public class Main implements Runnable{
+
+  class Point {
     int x;
     int y;
     int alt;
+    Point from;
+    int step;
+    boolean inPath;
 
     Point() {}
 
@@ -19,30 +23,31 @@ public class Main {
 
     @Override
     public String toString() {
-      return String.format("%d:%d/%d", x, y , alt);
+      return String.format("%d/%d:%d", x, y , alt);
     }
   }
 
-  static Point[][] Matr;
-  static int N;
-  static int M;
-  static int K;
-  static Point From;
-  static Point To;
+  Point[][] Matr;
+  int N;//количество х(столбцов)
+  int M;//количество y(строк)
+  int K;
+  Point pFrom;
+  Point pTo;
+  ArrayList<Point> path = new ArrayList<Point>();
 
-  public static void readDataFromFile() throws IOException {
+  public void readDataFromFile() throws IOException {
     BufferedReader in = new BufferedReader(new FileReader("in.txt"));
     String s;
     s = in.readLine();
     String[] leksArr = s.split(" ");
     N = Integer.parseInt(leksArr[0]);
     M = Integer.parseInt(leksArr[1]);
-    Matr = new Point[N][M];
-    for (int i=0; i<N; i++) {
-      for (int j=0; j<M; j++) {
-        Matr[i][j] = new Point();
-        Matr[i][j].x =i+1;
-        Matr[i][j].y =j+1;
+    Matr = new Point[M][N];
+    for (int y=0; y<M; y++) {
+      for (int x=0; x<N; x++) {
+        Matr[y][x] = new Point();
+        Matr[y][x].x = x;
+        Matr[y][x].y = y;
       }
     }
     int cntN = N;
@@ -56,33 +61,144 @@ public class Main {
     K = Integer.parseInt(in.readLine());
     s = in.readLine();
     leksArr = s.split(" ");
-    From = new Point();
-    From.x = Integer.parseInt(leksArr[0]);
-    From.y = Integer.parseInt(leksArr[1]);
-    To = new Point();
-    To.x = Integer.parseInt(leksArr[0]);
-    To.y = Integer.parseInt(leksArr[1]);
+    pFrom = Matr[Integer.parseInt(leksArr[1])-1][Integer.parseInt(leksArr[0])-1];
+    pTo = Matr[Integer.parseInt(leksArr[3])-1][Integer.parseInt(leksArr[2])-1];
     in.close();
   }
 
-  public static void printMatr(String comment) {
+  public void printMatrCoord(String comment) {
     System.out.printf("%s\n--------------\n", comment);
-    for (int i=0; i<N; i++) {
-      for (int j=0; j<M; j++) {
-        System.out.printf("%s ", Matr[i][j]);
+    for (int y=0; y<M; y++) {
+      for (int x=0; x<N; x++) {
+        System.out.printf("%s ", Matr[y][x]);
+      }
+      System.out.println();
+    }
+    System.out.printf("From %s\n", pFrom);
+    System.out.printf("To %s\n", pTo);
+    System.out.println();
+  }
+
+  public void printMatrStep(String comment) {
+    System.out.printf("%s\n--------------\n", comment);
+    for (int y=0; y<M; y++) {
+      for (int x=0; x<N; x++) {
+        Point p = Matr[y][x];
+        String s = String.format(p.inPath ? "%-2d● " : "%-3d ", p.step);
+        System.out.print(s);
       }
       System.out.println();
     }
   }
 
-  public static void main(String[] args) {
+  public Point getLeft(Point p) {
+    if (p.x == 0) {
+      return null;
+    }
+    else {
+      return Matr[p.y][p.x-1];
+    }
+  }
+
+  public Point getUp(Point p) {
+    if (p.y == 0) {
+      return null;
+    }
+    else {
+      return Matr[p.y-1][p.x];
+    }
+  }
+
+  public Point getRight(Point p) {
+    if (p.x == N-1) {
+      return null;
+    }
+    else {
+      return Matr[p.y][p.x+1];
+    }
+  }
+
+  public Point getDown(Point p) {
+    if (p.y == M-1) {
+      return null;
+    }
+    else {
+      return Matr[p.y+1][p.x];
+    }
+  }
+
+  public void setStep(Point p, int step, Point from) {
+    if (p != null && p.step==0) {
+      p.step = step + Math.abs(p.alt-from.alt);
+      p.from = from;
+    }
+  }
+
+  public void calc() {
+    int step=1;
+    boolean done = false;
+    pFrom.step=step++;
+    while (!done) {
+      for (int y=0; y<M && !done; y++) {
+        for (int x=0; x<N && !done; x++) {
+          Point p = Matr[y][x];
+          if (p.step != step-1)
+            continue;
+          Point pl = getLeft(p);
+          Point pr = getRight(p);
+          Point pu = getUp(p);
+          Point pd = getDown(p);
+          setStep(pl, step, p);
+          setStep(pd, step, p);
+          setStep(pu, step, p);
+          setStep(pr, step, p);
+          if (pl == pTo || pr == pTo || pu == pTo || pd == pTo) {
+            done = true;
+          }
+        }
+      }
+      step++;
+    }//while
+    if (done) {
+      Point p = pTo;
+      while (p != null) {
+        path.add(0, p);
+        p.inPath = true;
+        p = p.from;
+      }
+    }
+  }
+
+  public void printPath() {
+    for (int i=0; i<path.size(); i++) {
+      System.out.printf("%d)%s %s", path.get(i).step, path.get(i), i<path.size()-1 ? "-> " : "\n");
+    }
+  }
+
+  public void writeDataToFile() throws IOException {
+    BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
+    out.write(Integer.toString(pTo.step));
+    out.close();
+  }
+
+  @Override
+  public void run() {
     try {
       readDataFromFile();
-      printMatr("Source matr:");
-      System.out.println(K);
-      System.out.printf("From: %s To: %s", From, To);
+      printMatrCoord("Source matr:");
+      calc();
+      printMatrStep("Steps:");
+      printPath();
+      System.out.printf("pFrom: %s pTo: %s", pFrom, pTo);
+      writeDataToFile();
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public static void main(String[] args)  {
+    Thread t = new Thread(null, new Main(), "",2  * 1024 * 1024);
+    t.setPriority(10);
+    t.start();
   }
 }
