3584eb862914145b1e5fb7bd38913f06ebe11526
Gleb Mashkanov <gleb.mashkanov@gmail.com>
2018-05-10 20:05:03 +0300

    LR2 prinyato

--- LR2/src/Main.java
+++ LR2/src/Main.java
@@ -1,34 +1,33 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.TreeSet;
 
 public class Main {
 
   static int N;
-  static int[] mArr;
-  static TreeSet<String> set = new TreeSet<String>();
+  //static int[] mArr;
+  static int result;
+  //static TreeSet<String> set = new TreeSet<String>();
+  static HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
   static boolean DEBUG = false;
 
 
   public static void readDataFromFile() throws IOException {
     BufferedReader in = new BufferedReader(new FileReader("input.txt"));
     N = Integer.parseInt(in.readLine());
-    mArr = new int[N];
+   /* mArr = new int[N];
     for (int i=0; i<N; i++) {
       mArr[i] = i+1;
-    }
+    }*/
     in.close();
   }
-
+/*
   public static void reduce(int[] arr, int tr, int level) {
     int n = arr.length / 2;
     if (arr.length%2!=0)
       n+=tr;
     int[] resArr = new int[n];
     StringBuilder sb = new StringBuilder();
-    /*sb.append(tr);
-    for (int i=0; i<level; i++) {
-      sb.append(".");
-    }*/
     for (int i=0;i<resArr.length; i++) {
       resArr[i] = tr==1 ? arr[2*i] : arr[2*i+1];
       sb.append(String.format("%d ", resArr[i]));
@@ -42,25 +41,39 @@ public class Main {
       return;
     }
     set.add(s);
+  }*/
+
+  public static int calc(int n) throws Exception {
+    if (n<3)
+      throw new Exception("<3!!!!!!!!!!!!!");
+    int res = 0;
+    if (cache.containsKey(n)) {
+      res = cache.get(n);
+      return res;
+    }
+    res = calc(n/2 + n%2 ) + calc(n/2);
+    cache.put(n, res);
+    return res;
   }
 
   public static void writeDataToFile() throws IOException {
+    System.out.println(result);
     BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-    writer.write(Integer.toString(set.size()));
+    writer.write(Integer.toString(result));
     writer.close();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     long l = System.currentTimeMillis() ;
     try {
       readDataFromFile();
-      reduce(mArr, 0, 0);
-      reduce(mArr, 1, 0);
-      if (DEBUG) {
-        System.out.println("------------");
-        System.out.println(set);
-      }
-      System.out.println(set.size());
+      //reduce(mArr, 0, 0);
+      //reduce(mArr, 1, 0);
+      cache.put(3, 1);
+      cache.put(4, 2);
+      cache.put(5, 2);
+      cache.put(6, 2);
+      result = calc(N);
       writeDataToFile();
     } catch (IOException e) {
       e.printStackTrace();
